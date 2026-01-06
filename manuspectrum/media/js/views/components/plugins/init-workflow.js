486d6d635f372e890dc85709e363df863d981c73
rayondemiel <maxime.humeau@mnhn.fr>
2026-01-06 16:06:52 +0000

    init workflows update

--- manuspectrum/media/js/views/components/plugins/init-workflow.js
+++ manuspectrum/media/js/views/components/plugins/init-workflow.js
@@ -2,88 +2,85 @@
 // GPL-3.0 license
 // https://github.com/archesproject/arches-for-science
 
-define([
-    'knockout',
-    'arches',
-    'viewmodels/alert-json',
-    'templates/views/components/plugins/init-workflow.htm'
-], function(ko, arches, JsonErrorAlertViewModel, initWorkflowTemplate) {
+import ko from 'knockout';
+import arches from 'arches';
+import JsonErrorAlertViewModel from 'viewmodels/alert-json';
+import initWorkflowTemplate from 'templates/views/components/plugins/init-workflow.htm';
 
-    var InitWorkflow = function(params) {
-        this.workflows = ko.observableArray([]);
-        this.helpTemplateData = ko.observableArray([]);
+const InitWorkflow = function(params) {
+    this.workflows = ko.observableArray([]);
+    this.helpTemplateData = ko.observableArray([]);
 
-        fetch(arches.urls.api_plugins).then(resp => {
-            if (resp.ok) {
-                return resp.json();
+    // Collapsible groups - open by default
+    this.showProjectsGroup = ko.observable(true);
+
+    fetch(arches.urls.api_plugins).then((resp) => {
+        if (resp.ok) {
+            return resp.json();
+        } else {
+            params.alert(new JsonErrorAlertViewModel('ep-alert-red', resp.responseJSON));
+        }
+    }).then((respJSON) => {
+        const workflows = respJSON.reduce((acc, plugin) => {
+            if (plugin.config.is_workflow) {
+                plugin.url = arches.urls.plugin(plugin.slug);
+                acc.push(plugin);
             }
-            else {
-                params.alert(new JsonErrorAlertViewModel('ep-alert-red', resp.responseJSON));
+            return acc;
+        }, []);
+
+        this.workflows(workflows);
+        this.helpTemplateData(workflows.reduce((acc, workflow) => {
+            if (workflow.helptemplate) {
+                acc.push({ text: workflow.name, id: workflow.helptemplate });
             }
-        }).then(respJSON => {
-            let workflows = respJSON.reduce((acc, plugin) => {
-                if (plugin.config.is_workflow) {
-                    plugin.url = arches.urls.plugin(plugin.slug);
-                    acc.push(plugin);
-                }
-                return acc;
-            }, []);
-
-            this.workflows(workflows);
-            this.helpTemplateData(workflows.reduce((acc, workflow) => {
-                if (workflow.helptemplate) {
-                    acc.push({'text': workflow.name, 'id': workflow.helptemplate});
-                }
-
-                return acc;
-            }, []));
-        });
-
-        this.shouldShowWorkflowHelp = ko.observable(false);
-        this.helpTemplateUrl = ko.observable();
-        this.isHelpTemplateLoading = ko.observable();
-        this.selectedHelpTemplate = ko.observable();
-        this.selectedHelpTemplate.subscribe(helpTemplateName => {
-            if (helpTemplateName) {
-                this.isHelpTemplateLoading(true);
-                this.helpTemplateUrl(arches.urls.help_template + `?template=${helpTemplateName}`);
-            }
-            else {
-                this.helpTemplateUrl(null);
-            }
-        });
-
-        this.shouldShowIncompleteWorkflowsModal = ko.observable(false);
-        this.requestingUserIsSuperuser = ko.observable(false);
-
-        this.incompleteWorkflows = ko.observableArray([]);
-        this.incompleteWorkflows.subscribe(incompleteWorkflows => {
-            if (incompleteWorkflows.length) {
-                this.shouldShowIncompleteWorkflowsModal(true);
-            }
-        });
-
-        fetch(arches.urls.api_user_incomplete_workflows).then(resp => {
-            if (resp.ok) {
-                return resp.json();
-            }
-            else {
-                params.alert(new JsonErrorAlertViewModel('ep-alert-red', resp.responseJSON));
-            }
-        }).then(respJSON => {
-            this.incompleteWorkflows(respJSON['incomplete_workflows'].map(workflowData => {
-                const datetime = new Date(workflowData['created']);
-                workflowData['created'] = datetime.toLocaleString();
-
-                return workflowData;
-            }));
-
-            this.requestingUserIsSuperuser(respJSON['requesting_user_is_superuser']);
-        });
-    };
-
-    return ko.components.register('init-workflow', {
-        viewModel: InitWorkflow,
-        template: initWorkflowTemplate
+            return acc;
+        }, []));
     });
+
+    this.shouldShowWorkflowHelp = ko.observable(false);
+    this.helpTemplateUrl = ko.observable();
+    this.isHelpTemplateLoading = ko.observable();
+    this.selectedHelpTemplate = ko.observable();
+    this.selectedHelpTemplate.subscribe((helpTemplateName) => {
+        if (helpTemplateName) {
+            this.isHelpTemplateLoading(true);
+            this.helpTemplateUrl(arches.urls.help_template + `?template=${helpTemplateName}`);
+        } else {
+            this.helpTemplateUrl(null);
+        }
+    });
+
+    this.shouldShowIncompleteWorkflowsModal = ko.observable(false);
+    this.requestingUserIsSuperuser = ko.observable(false);
+
+    this.incompleteWorkflows = ko.observableArray([]);
+    this.incompleteWorkflows.subscribe((incompleteWorkflows) => {
+        if (incompleteWorkflows.length) {
+            this.shouldShowIncompleteWorkflowsModal(true);
+        }
+    });
+
+    fetch(arches.urls.api_user_incomplete_workflows).then((resp) => {
+        if (resp.ok) {
+            return resp.json();
+        } else {
+            params.alert(new JsonErrorAlertViewModel('ep-alert-red', resp.responseJSON));
+        }
+    }).then((respJSON) => {
+        this.incompleteWorkflows(respJSON.incomplete_workflows.map((workflowData) => {
+            const datetime = new Date(workflowData.created);
+            workflowData.created = datetime.toLocaleString();
+            return workflowData;
+        }));
+
+        this.requestingUserIsSuperuser(respJSON.requesting_user_is_superuser);
+    });
+};
+
+ko.components.register('init-workflow', {
+    viewModel: InitWorkflow,
+    template: initWorkflowTemplate
 });
+
+export default InitWorkflow;
