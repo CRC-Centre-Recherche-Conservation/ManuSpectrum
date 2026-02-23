9511d30d736cb5681896e21220d565ab4ddaca18
rayondemiel <maxime.humeau@mnhn.fr>
2026-02-23 10:40:21 +0000

    fix test settings

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -64,6 +64,9 @@ LOGGING["loggers"]["arches"]["level"] = "ERROR"
 
 ELASTICSEARCH_PREFIX = "test"
 
+ELASTICSEARCH_HOSTS = [{"scheme": "http", "host": "localhost", "port": 9200}]
+ELASTICSEARCH_CONNECTION_OPTIONS = {"request_timeout": 30}
+
 TEST_RUNNER = "arches.test.runner.ArchesTestRunner"
 SILENCED_SYSTEM_CHECKS.append(
     "arches.W001",  # Cache backend does not support rate-limiting
